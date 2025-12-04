cadd9a9259d94f56ddef1e4b9bc1baf2a94084d8
NatalieMalaskova <malaskovan.07@spst.eu>
2025-12-04 02:00:20 +0100

    idk

--- game/Assets/scripts/movement.cs
+++ game/Assets/scripts/movement.cs
@@ -5,10 +5,12 @@ public class movement : MonoBehaviour
     public float moveSpeed = 5f; //speed
     private Rigidbody2D rb;
     private Vector2 moveInput;
+    private Animator anim;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void Update()
@@ -17,6 +19,12 @@ public class movement : MonoBehaviour
         moveInput.x = Input.GetAxisRaw("Horizontal");
         moveInput.y = Input.GetAxisRaw("Vertical");
         moveInput.Normalize(); //diagonalni pohyb nebude rychlejsi
+
+        anim.SetBool("moving", moveInput != Vector2.zero);
+        if (moveInput.x > 0) anim.SetInteger("direction", 3);     // right
+        else if (moveInput.x < 0) anim.SetInteger("direction", 2); // left
+        else if (moveInput.y > 0) anim.SetInteger("direction", 1); // up
+        else if (moveInput.y < 0) anim.SetInteger("direction", 0); // down
     }
 
     void FixedUpdate()
@@ -24,3 +32,4 @@ public class movement : MonoBehaviour
         rb.MovePosition(rb.position + moveInput * moveSpeed * Time.fixedDeltaTime);
     }
 }
+
